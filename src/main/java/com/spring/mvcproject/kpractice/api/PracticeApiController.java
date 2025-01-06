e0276f81395673ca7738de17c8f37cdd9c2efa49
rlawjdgkr <irlawjdgkr@naver.com>
2025-01-06 16:25:43 +0900

    연습 2

--- src/main/java/com/spring/mvcproject/kpractice/api/PracticeApiController.java
+++ src/main/java/com/spring/mvcproject/kpractice/api/PracticeApiController.java
@@ -1,10 +1,8 @@
 package com.spring.mvcproject.kpractice.api;
 
 import com.spring.mvcproject.kpractice.practice.Practice;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -34,6 +32,33 @@ public class PracticeApiController {
     public List<Practice> getPractices() {
         return new ArrayList<>(practiceStore.values());
     }
+
+   // 개별 조회 API
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findOne(@PathVariable Long id) {
+        Practice practice = practiceStore.get(id);
+        if (practice == null) {
+            return ResponseEntity.notFound().build();
+
+        }
+        return ResponseEntity.ok().body(practice);
+    }
+
+    // 삭제 API
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> Delete(@PathVariable Long id){
+        practiceStore.remove(id);
+        return ResponseEntity.ok().body("삭제가 완료되었습니다");
+
+    }
+
+    //추가 API
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Practice practice){
+        practice.setId(nextId++);
+        practiceStore.put(practice.getId(), practice);
+    return ResponseEntity.ok().body("생성완료");
+  }
 
 
 }
--- src/main/webapp/WEB-INF/views/practice/practice.jsp
+++ src/main/webapp/WEB-INF/views/practice/practice.jsp
@@ -26,14 +26,37 @@
 <body>
 <h1>MVC 댄서 정보 목록</h1>
 <ul id="dancer-list">
-    <li>
-        # 이름: <span class="dancer-name">xxx</span>,
-        # 크루명: xxx,
-        # 수준: xxx
-        <button class="del-btn">삭제</button>
-    </li>
+
 </ul>
 
 <a href="/mvc/v1/register">다시 등록하기</a>
+<script>
+
+    function renderPracticeList(data){
+     const $dancerList  = document.getElementById('dancer-list');
+     data.forEach(({id,name,age}) =>{
+          $dancerList.innerHTML += `
+            <li>
+           # 아이디: <span class="dancer-name">\${id}</span>,
+        # 이름: \${name},
+        # 나이: \${age}
+          </li>`
+     });
+
+    }
+    async function getApi(){
+        const res = await fetch("/api/v1/practice");
+        const data = await res.json();
+
+        // 화면에 정보 랜더링
+        renderPracticeList(data);
+    }
+
+
+
+    // 실행 코드
+    getApi();
+
+</script>
 </body>
 </html>
